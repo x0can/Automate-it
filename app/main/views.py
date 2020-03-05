0beda8b52b43cd6d7eaca98fad45dac4e7f49e1f
alexmwaura <alexmwaura43@gmail.com>
2020-03-05 21:35:31 +0300

    captcher key error

--- app/main/views.py
+++ app/main/views.py
@@ -9,32 +9,34 @@ from flask_user import current_user, login_required, roles_required, UserManager
 
 @main.route("/post_field", methods=["GET", "POST"])
 def need_input():
-    result = request.form.to_dict(flat=False)
-    result = {
-        key: value[0] if len(value)== 1 else value
-        for key, value in request.form.items()
-    }
 
-    if result['owner']=="":
-        result['owner']==result['driverName']
+    try:
+        result = request.form.to_dict(flat=False)
+        result = {
+            key: value[0] if len(value)== 1 else value
+            for key, value in request.form.items()
+        }
 
-        details = Detail(model=result["model"], driver_name=result["driverName"],
-        owner_name=result["owner"],owner_email=result["email"],company_name= result["company"],
-        eng_no =result["engNo"], reg_no=result["regNo"], mileage=result["mileage"],driver_no=result["driverNo"]
-        )
-        db.session.add(details)
-        db.session.commit()
-        data = details.query.filter_by(id=details.id).first()
-        print(data)
-    else:
-        details = Detail(model=result["model"], driver_name=result["driverName"],
-        owner_name=result["owner"],owner_email=result["email"],company_name= result["company"],
-        eng_no =result["engNo"], reg_no=result["regNo"], mileage=result["mileage"],driver_no=result["driverNo"]
-        )
-        db.session.add(details)
-        db.session.commit()
-        data = details.query.filter_by(id=details.id).first()
-        
+        if result['owner']=="":
+            result['owner']==result['driverName']
+
+            details = Detail(model=result["model"], driver_name=result["driverName"],
+            owner_name=result["owner"],owner_email=result["email"],company_name= result["company"],
+            eng_no =result["engNo"], reg_no=result["regNo"], mileage=result["mileage"],driver_no=result["driverNo"]
+            )
+            db.session.add(details)
+            db.session.commit()
+            data = details.query.filter_by(id=details.id).first()
+            print(data)
+        else:
+            details = Detail(model=result["model"], driver_name=result["driverName"],
+            owner_name=result["owner"],owner_email=result["email"],company_name= result["company"],
+            eng_no =result["engNo"], reg_no=result["regNo"], mileage=result["mileage"],driver_no=result["driverNo"]
+            )
+            db.session.add(details)
+            db.session.commit()
+            data = details.query.filter_by(id=details.id).first()
+            
         owner = result["owner"]
         drivername= result['driverName']
         email = result["email"]
@@ -44,18 +46,17 @@ def need_input():
 
 
         context = {
-            "owner":owner,
-            "driver":drivername,
-            "email":email,
-            "mileage":mileage,
-            "company":company,
-        }
+                "owner":owner,
+                "driver":drivername,
+                "email":email,
+                "mileage":mileage,
+                "company":company,
+            }
+        return render_template('thanks.html',data = data, result=result,context=context)
         
-
-
-    return render_template('thanks.html',data = data, result=result,context=context,owner=owner)
-
-
+    except KeyError:
+        return render_template("fourOwfour.html")
+    
 
 
 
