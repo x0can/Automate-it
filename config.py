7edaa911915f0167997c9e059914523ac238dbfa
alexmwaura <alexmwaura43@gmail.com>
2020-03-05 20:34:27 +0300

    debug

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     '''
