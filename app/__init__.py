d66cf3b3cec8303db83bee730c8b7e30e7d8b33c
alexmwaura <alexmwaura43@gmail.com>
2020-03-05 17:08:11 +0300

    Initialize the application

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy() 
+
+# user_manager = UserManager
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+
+
+   
+    app.config.from_object(config_options[config_name])
+    from .models import User
+
+    
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
+    from .main import main as main_blueprint
+    
+
+    app.register_blueprint(main_blueprint)
+    with app.app_context():
+        from .main import views
+        db.create_all()
+
+
+        return app
+
