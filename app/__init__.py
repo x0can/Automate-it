81fa8dafa4c5719b66b95847b1a7ec9d84ba8c2c
alexmwaura <alexmwaura43@gmail.com>
2020-03-06 06:16:57 +0300

    update initialization for flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,28 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_user import SQLAlchemyAdapter
+from flask_login import LoginManager
 
 db = SQLAlchemy() 
 
-# user_manager = UserManager
+login_manager = LoginManager()
 
 def create_app(config_name):
     
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
-
-
    
     app.config.from_object(config_options[config_name])
     from .models import User
+    # db_adapter = SQLAlchemyAdapter(db, User)
+    # user_manager = UserManager(db_adapter)
+    login_manager.init_app(app)
 
-    
 
     db.init_app(app)
+
+    # user_manager.init_app(app)
     
     from .main import main as main_blueprint
     
@@ -30,7 +31,14 @@ def create_app(config_name):
     # with app.app_context():
     #     from .main import views
     #     db.create_all()
+    @login_manager.user_loader
+    def user_loader(user_id):
+        """Given *user_id*, return the associated User object.
 
+        :param unicode user_id: user_id (email) user to retrieve
+
+        """
+        return User.query.get(user_id)
 
     return app
 
